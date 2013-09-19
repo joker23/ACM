08744e4c5dbc0139de443ea97ed99d0706607d24
steven zhang <steven@localhost.localdomain>
2013-09-19 17:36:30 -0400

    removed things

--- gatech_sep_12_13/pacnw2012/SarumansLevelUp/validate.pl
+++
@@ -1,7 +0,0 @@
-while (<>) {
-   chomp ;
-   $lin = $_ ;
-   $n = - - $lin ;
-   die "Bad integer" if $n <= 0 || $n > 1e16 ;
-   die "Bad format" if $lin ne "$lin" ;
-}
--- gatech_sep_12_13/pacnw2012/Tongues/validate.pl
+++
@@ -1,5 +0,0 @@
-while (<>) {
-   chomp ;
-   die "Too long an input line" if length($_) > 100 ;
-   die "Bad chars" if $_ !~ /^[ -~]*$/ ;
-}
