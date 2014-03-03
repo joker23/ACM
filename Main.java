7f5726873edc253bcfa02b859e64ba92427a0dcd
joker23 <zhangksteven@gmail.com>
2014-03-03 00:12:50 -0500

    solved one to test

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+import java.awt.Point;
+
+import static java.util.Arrays.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Long.*;
+import static java.lang.Short.*;
+import static java.lang.Math.*;
+import static java.math.BigInteger.*;
+import static java.util.Collections.*;
+
+public class Main {
+
+	private BufferedReader in;
+	private StringTokenizer st;
+	private PrintWriter out;
+
+	public void solve() throws Exception {
+
+	}
+
+	public Main() {
+		this.in = new BufferedReader(new InputStreamReader(System.in));
+		this.out = new PrintWriter(System.out);
+	}
+
+	public void end() {
+		try {
+			this.out.flush();
+			this.out.close();
+			this.in.close();
+		} catch (Exception e){
+			//do nothing then :)
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Main solver = new Main();
+		solver.solve();
+		solver.end();
+	}
+}
